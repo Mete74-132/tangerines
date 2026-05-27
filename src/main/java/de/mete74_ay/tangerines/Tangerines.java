c1f383c25ecae995356df49e2bc55fd43a6bd699
Mete74-132 <geomete74@gmail.com>
2026-05-27 13:00:27 +0200

    just uploading

---
+++ src/main/java/de/mete74_ay/tangerines/Tangerines.java
@@ -0,0 +1,16 @@
+package de.mete74_ay.tangerines;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Tangerines implements ModInitializer {
+	public static final String MOD_ID = "tangerines";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+}
